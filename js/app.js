92f7e35be8a3207943b2011caa9406ff0d2d2662
jorge lopez <emanuelbatista5null@gmail.com>
2016-09-05 21:17:02 -0400

    tarea 5

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+(function () {
+//esto es una funcion anonima
+//y autoejecutable.
+  console.log('Hola mundo'); //impresion en consola
+
+
+
+}());
+var miarray = []; // esto es una varible tipo array
+var letra = 'MiString'; //una variable tipo string
+var minumero = 3; //variable numerica
+miarray.push('Primer elemento'); //elemento de array
+miarray.push('Segundo elemento'); //elemento de array
+console.log(miarray); //impresion en consola de array
+var miObjeto = {}; // variable tipo object
+miObjeto.descripcion = 'zapatillas'; //atributos
+miObjeto.cantidad = 12; //atributos
+console.log(miObjeto);
